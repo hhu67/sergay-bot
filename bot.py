e634142f2d15c5e0acb912f4696177b6aece8deb
hhu67 <hhu67@hhu67.pw>
2026-08-16 01:32:17 +0300

    Update add added new phrase

--- bot.py
+++ bot.py
@@ -6,6 +6,11 @@ from dotenv import load_dotenv
 from aiogram import Dispatcher, Bot, F
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
+class FormNewPhrase(StatesGroup):
+    phrase = State()
 
 load_dotenv()
 conn = sqlite3.connect("bot.sqlite3")
@@ -16,7 +21,8 @@ TGBOT_TOKEN = os.getenv("TGBOT")
 bot = Bot(token=TGBOT_TOKEN)
 dp = Dispatcher()
 start_text = """Жми:
-/phrase"""
+/phrase
+/new"""
 
 @dp.message(CommandStart())
 async def start(message: Message):
@@ -27,6 +33,20 @@ async def phrase(message: Message):
     cursor.execute("SELECT phrase FROM sergay_bot ORDER BY RANDOM() LIMIT 1")
     phrase = cursor.fetchone()[0]
     await message.answer(f"серГЕЙ {phrase}")
+
+@dp.message(Command("new"))
+async def new(message: Message, state: FSMContext):
+    await state.set_state(FormNewPhrase.phrase)
+    await message.answer("Назови гандона по новому")
+
+@dp.message(FormNewPhrase.phrase)
+async def new_insert(message: Message, state: FSMContext):
+    user_data = await state.get_data()
+    phrase = message.text
+    cursor.execute("INSERT INTO sergay_bot (phrase) VALUES (?)", (phrase,))
+    conn.commit()
+    await state.clear()
+    await message.answer(f"Успешно добавленно серГЕЙ {phrase}")
 
 @dp.message(F.text.startswith("/"))
 async def all_no_command(message: Message):
